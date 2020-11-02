b81bbad09e25fa7befa1f142b41e381a147b380d
Sam Morrell <smorrel1@my.westga.edu>
2020-11-01 22:01:47 -0500

    Created controller class and extensions

---
+++ Covid19Analysis/Extensions/CollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Analysis.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
+        {
+            return new ObservableCollection<T>(collection);
+        }
+    }
+}
---
+++ Covid19Analysis/ViewModel/CovidDataController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Covid19Analysis.Annotations;
+using Covid19Analysis.Extensions;
+using Covid19Analysis.Model;
+
+namespace Covid19Analysis.ViewModel
+{
+    class CovidDataController : INotifyPropertyChanged
+
+    {
+    private CovidDataCollection covidDataCollection;
+
+    private ObservableCollection<CovidData> covidDatas;
+
+    public ObservableCollection<CovidData> CovidDatas
+    {
+        get { return covidDatas; }
+        set { covidDatas = value; }
+
+    }
+
+    private CovidData selectedCovidData;
+
+    public CovidData SelectedCovidData
+    {
+        get { return selectedCovidData; }
+        set
+        {
+            selectedCovidData = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public CovidDataController()
+    {
+        this.covidDataCollection = new CovidDataCollection();
+
+        this.covidDatas = this.covidDataCollection.ToObservableCollection();
+    }
+
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    }
+}
